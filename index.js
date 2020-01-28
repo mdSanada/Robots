9f380c8d56e63752a657ede2ddc4ce22f796ef67
mdSanada <maatheusdavid@me.com>
2020-01-28 20:33:45 -0300

    Node install + Readline sync + Search term

---
+++ index.js
@@ -0,0 +1,13 @@
+const readLine = require('readline-sync')
+
+function start() {
+    const content = {}
+    content.searchTerm = askAndReturnSearchTerm()
+
+    function askAndReturnSearchTerm() {
+        return readLine.question('Type a Wikipedia search term: ')
+    }
+
+    console.log(content)
+}
+start()
