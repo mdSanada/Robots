428b9cef90f44fa758ed891a40165f70b57040d0
mdSanada <maatheusdavid@me.com>
2020-01-28 21:31:54 -0300

    API Algorithmia + Search text in Wikipedia

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const readLine = require('readline-sync')
+const robots = {
+    //userInput: require('./robots/user-input.js'),
+    text: require('./robots/text.js')
+}
 
 function start() {
     const content = {}
     content.searchTerm = askAndReturnSearchTerm()
     content.prefix = askAndReturnPrefix()
+
+    //robots.userInput(content)
+    robots.text(content)
 
     function askAndReturnSearchTerm() {
         return readLine.question('Type a Wikipedia search term: ')
